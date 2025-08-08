a6b938bb8ba099204409a6d0ef8be2eda4bb82af
AYUSH KUMAR <AYUSH KUMAR@AyushKumarSingh>
2025-08-08 11:35:14 +0530

    sync fork

---
+++ SampleMaven/src/main/java/TESTINGFORK/learnsyncfork.java
@@ -0,0 +1,10 @@
+package TESTINGFORK;
+
+public class learnsyncfork {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("leanring sync fork");
+	}
+
+}
